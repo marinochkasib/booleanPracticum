1f7a6ad03b487522367b8368550e9ad5980e2d75
marinochkasib <102374297+marinochkasib@users.noreply.github.com>
2024-09-07 09:21:51 +0300

    Add files via upload

---
+++ index.java
@@ -0,0 +1,17 @@
+class Practicum {
+	public static void main(String[] args) {
+		 int megabytes = 64000;
+		 int gigabytes = 64;
+		 int megabytesPerGigabyte = 1024;
+		 int gigabytesToMegabytes = gigabytes * megabytesPerGigabyte; // напечатает false
+		 
+		 boolean isMbMore = megabytes >= gigabytesToMegabytes;
+
+		 class Practicum {
+			public static void main(String[] args) {
+				 boolean result = 5 > 3;
+				 System.out.println(result); // напечатает true
+			}
+	  }
+
+		 //Файлы с расширением .java - исходники классов, .class - скомпилированные в байткоды классы, .jar - это обычный архив, типо zip, только как бы джавовский.
